2068b4eaa4787422d6daaf2e7a1b6ea6a1ce48bb
Eric Anderson <splanderson@gmail.com>
2016-11-14 08:13:22 -0700

    Added get endpoints

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var massive = require('massive');
-var massiveInstance = massive.connectSync({
-  connectionString : "postgres://postgres:1234@localhost:8666/bungee"
-});
-// var controller = require('./controller.js');
+const express = require('express')
+  ,bodyParser = require('body-parser')
+  ,cors = require('cors')
+  ,massive = require('massive')
+  ,massiveInstance = massive.connectSync({
+    connectionString : "postgres://postgres:1234@localhost:8666/bungee"
+  });
+var mainCtrl = require('./public/mainCtrl.js');
 
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
 app.set('db', massiveInstance);
+
+//GET read_link.sql
+app.get('/search', mainCtrl.getAll);
+//GET return_link.sql
+// app.get('/categories', mainCtrl.getAll);
+
+//PUT update_link.sql
+
 
 app.listen(8080, function(){
   console.log("Successfully listening on : 8080")
